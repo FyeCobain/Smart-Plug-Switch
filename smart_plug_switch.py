d8d5ff3d4bb6a101551feedb36152e7551e85eed
Fye <hereisnirvana@hotmail.com>
2024-02-27 10:45:36 -0600

    1.0.1

--- smart_plug_switch.py
+++ smart_plug_switch.py
@@ -1,13 +1,9 @@
-import tinytuya
 from sys import argv, exit
+import tinytuya
 
 if (len(argv) < 5):
     exit()
 
 d = tinytuya.OutletDevice(argv[1], argv[2], argv[3])
 d.set_version(float(argv[4]))
-
-if argv[5].strip().lower() == 'on':
-    d.turn_on()
-else:
-    d.turn_off()
+d.turn_on() if argv[5].strip().lower() == 'on' else d.turn_off()
